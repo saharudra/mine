ae35fb78cf2cf3943b3cf5ac5e4855a2d03e6ab1
saharudra <rudra.saha@asu.edu>
2019-04-08 11:09:14 -0400

    Mine

--- mine_net_vanilla.py
+++ mine_net_vanilla.py
@@ -64,6 +64,7 @@ def mutual_information(joint, marginal, mine_net):
     mi_lb = torch.mean(t) - torch.log(torch.mean(et))
     return mi_lb, t, et
 
+
 def learn_mine(batch, mine_net, mine_net_optim,  ma_et, ma_rate=0.01):
     # batch is a tuple of (joint, marginal)
     joint , marginal = batch
@@ -81,6 +82,25 @@ def learn_mine(batch, mine_net, mine_net_optim,  ma_et, ma_rate=0.01):
     loss.backward()
     mine_net_optim.step()
     return mi_lb, ma_et
+
+
+def train(data, mine_net,mine_net_optim, batch_size=100, iter_num=int(5e+3), log_freq=int(1e+3)):
+    # data is x or y
+    result = list()
+    ma_et = 1.
+    for i in range(iter_num):
+        batch = sample_batch(data,batch_size=batch_size)\
+        , sample_batch(data,batch_size=batch_size,sample_mode='marginal')
+        mi_lb, ma_et = learn_mine(batch, mine_net, mine_net_optim, ma_et)
+        result.append(mi_lb.detach().cpu().numpy())
+        if (i+1)%(log_freq)==0:
+            print(result[-1])
+    return result
+
+
+def ma(a, window_size=100):
+    return [np.mean(a[i:i+window_size]) for i in range(0,len(a)-window_size)]
+
 
 
 if __name__ == "__main__":
@@ -101,8 +121,24 @@ if __name__ == "__main__":
     # sns.scatterplot(x=sample_y[:, 0], y=sample_x[:, 1])
 
     joint_data = sample_batch(y,batch_size=100,sample_mode='joint')
-    sns.scatterplot(x=joint_data[:,0],y=joint_data[:,1],color='red')
+    # sns.scatterplot(x=joint_data[:,0],y=joint_data[:,1],color='red')
     marginal_data = sample_batch(y,batch_size=100,sample_mode='marginal')
-    sns.scatterplot(x=marginal_data[:,0],y=marginal_data[:,1])
-    plt.show()
+    # sns.scatterplot(x=marginal_data[:,0],y=marginal_data[:,1])
+    # plt.show()
 
+    mine_net_indep = Mine().cuda()
+    mine_net_optim_indep = optim.Adam(mine_net_indep.parameters(), lr=1e-3)
+    result_indep = train(x,mine_net_indep,mine_net_optim_indep)
+
+    result_indep_ma = ma(result_indep)
+    print(result_indep_ma[-1])
+    plt.plot(range(len(result_indep_ma)),result_indep_ma)
+
+    mine_net_cor = Mine().cuda()
+    mine_net_optim_cor = optim.Adam(mine_net_cor.parameters(), lr=1e-3)
+    result_cor = train(y,mine_net_cor,mine_net_optim_cor)
+
+
+    result_cor_ma = ma(result_cor)
+    print(result_cor_ma[-1])
+    plt.plot(range(len(result_cor_ma)),result_cor_ma)
